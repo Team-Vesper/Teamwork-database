04f54c779decb550682b2b59dda79d7886b5d819
Stoiko Neykov <stoikoneykov@gmail.com>
2016-10-27 11:12:06 +0300

    MongoDeveloper Model added

---
+++ TeamVesper/TeamVesper.MongoDB.Models/MongoDeveloper.cs
@@ -0,0 +1,25 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace TeamVesper.MongoDB.Models
+{
+    public class MongoDeveloper
+    {
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        public string Username { get; set; }
+
+        public string FullName { get; set; }
+
+        public int Age { get; set; }
+
+        public int Salary { get; set; }
+
+        public string Speciality { get; set; }
+
+        public string Team { get; set; }
+
+        public bool isTeamLeader { get; set; }
+    }
+}
