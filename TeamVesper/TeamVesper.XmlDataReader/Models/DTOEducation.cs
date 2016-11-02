262d761b59659b6d8af7eec6f1fa033afcb8602a
Samuil Mihaylov <samuil.a.mihaylov@gmail.com>
2016-11-02 12:29:16 +0200

    DTOEducation added in TeamVesper.XmlDataReader
    Data Transfer Object Education Model class added.

---
+++ TeamVesper/TeamVesper.XmlDataReader/Models/DTOEducation.cs
@@ -0,0 +1,26 @@
+﻿using System.Xml.Serialization;
+
+namespace TeamVesper.XmlDataReader.Models
+{
+    [XmlRoot(ElementName = "developperEducation")]
+    public class DTOEducation
+    {
+        [XmlAttribute("id")]
+        public int Id { get; set; }
+
+        [XmlAttribute("highSchoolName")]
+        public string HighSchoolName { get; set; }
+
+        [XmlAttribute("highSchoolTown")]
+        public string HighSchoolTown { get; set; }
+
+        [XmlAttribute("univerisityName")]
+        public string UniversityName { get; set; }
+
+        [XmlAttribute("univerisityTown")]
+        public string UniversityTown { get; set; }
+
+        [XmlAttribute("universityGradeNote")]
+        public int UniversityGradeNote { get; set; }
+    }
+}
