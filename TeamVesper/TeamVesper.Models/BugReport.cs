f391ef2aa1661b15d45ebe4f2822595703221e2c
Samuil Mihaylov <samuil.a.mihaylov@gmail.com>
2016-11-02 14:44:54 +0200

    BugReport model class added in TeamVesper.Models

---
+++ TeamVesper/TeamVesper.Models/BugReport.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace TeamVesper.Models
+{
+    public class BugReport
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public DateTime? AcceptedOn { get; set; }
+
+        public DateTime? SolvedOn { get; set; }
+    }
+}
