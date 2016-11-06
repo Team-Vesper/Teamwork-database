9b1eecabf9394d5e7e8c3696876f5bc43a471cb9
Stoiko Neykov <stoikoneykov@gmail.com>
2016-11-06 09:46:22 +0200

    creating dbs now async

--- TeamVesper/TeamVesper.UI/CreateDBForm.cs
+++ TeamVesper/TeamVesper.UI/CreateDBForm.cs
@@ -5,31 +5,39 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using TeamVesper.UI.Contracts;
 
 namespace TeamVesper.UI
 {
     public partial class CreateDBForm : Form
     {
-        public CreateDBForm()
+        private IDbInitializerFactory factory;
+
+        public CreateDBForm(IDbInitializerFactory factory)
         {
+            this.factory = factory;
             InitializeComponent();
         }
 
         private void CreateMongoDB_Click(object sender, EventArgs e)
         {
-            CreateMongoDbButton.Enabled = false;
+            Task task = new Task(() => this.InitMongoDb());
+            task.Start();
         }
 
         private void CreateSqlServer_Click(object sender, EventArgs e)
         {
-            CreateSQLServerButton.Enabled = false;
+            Task task = new Task(() => this.InitSqlServer());
+            task.Start();
         }
 
         private void CreateSQLite_Click(object sender, EventArgs e)
         {
-            CreateSQLiteButton.Enabled = false;
+            Task task = new Task(() => this.InitSQLite());
+            task.Start();
         }
 
         private void Back_Click(object sender, EventArgs e)
@@ -38,6 +46,27 @@ namespace TeamVesper.UI
 
             parent.Show();
             this.Close();
+        }
+
+        private async Task InitMongoDb()
+        {
+            var init = factory.GetMongoDbInitializer();
+            init.CreateDB();
+            await Task.Delay(1);
+        }
+
+        private async Task InitSqlServer()
+        {
+            var init = factory.GetSqlServerInitializer();
+            init.CreateDB();
+            await Task.Delay(1);
+        }
+
+        private async Task InitSQLite()
+        {
+            var init = factory.GetSQLiteInitializer();
+            init.CreateDB();
+            await Task.Delay(1);
         }
     }
 }
